76d90bfe4fc9f00cdef1c84c8ceadb0083d1efa1
Ahmet Enes Bayraktar <bayraktarahmetenes@gmail.com>
2025-08-01 11:57:51 +0300

    add launch configuration example for steam

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,22 @@
+import "dart:developer" as developer;
+import "dart:io";
+
 import "package:flame/game.dart";
 import "package:flutter/material.dart";
 
 import "game.dart";
 
-void main() {
+Future<void> main() async {
+  // Attempt to get the VM service URI and dump it to a file.
+  try {
+    final info = await developer.Service.getInfo();
+    final uri = info.serverUri; // may be null if service not yet available
+    if (uri != null) {
+      await File("vmservice_info.txt").writeAsString("{ \"uri\": \"$uri\" }");
+    }
+  } catch (e) {
+    // ignore or log somewhere safe
+  }
+
   runApp(GameWidget(game: GameInstance()));
 }
